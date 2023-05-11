51b97dcbcfe86645f38c3607395c6cd40de13a31
romer8 <gio.busrom@gmail.com>
2023-05-11 15:56:26 +0000

    tag version v0.0.1_0_2023_05_11

--- setup.py
+++ setup.py
@@ -1,31 +1,30 @@
 from setuptools import setup, find_namespace_packages
-from tethys_apps.app_installation import find_resource_files
+from setup_helper import find_all_resource_files
 
 # -- Apps Definition -- #
-app_package = 'swe'
-release_package = 'tethysapp-' + app_package
+namespace = 'tethysapp'
+app_package = "swe"
+release_package = "tethysapp-" + app_package
 
 # -- Python Dependencies -- #
 dependencies = []
 
 # -- Get Resource File -- #
-resource_files = find_resource_files('tethysapp/' + app_package + '/templates', 'tethysapp/' + app_package)
-resource_files += find_resource_files('tethysapp/' + app_package + '/public', 'tethysapp/' + app_package)
-resource_files += find_resource_files('tethysapp/' + app_package + '/workspaces', 'tethysapp/' + app_package)
+resource_files = find_all_resource_files(app_package, namespace)
 
 
 setup(
     name=release_package,
-    version='0.0.1',
-    description='',
-    long_description='',
-    keywords='',
-    author='Karnesh Jain',
-    author_email='kjain1@ua.edu',
-    url='',
-    license='MIT',
+    version="0.0.1",
+    description="",
+    long_description="",
+    keywords="replace_keywords",
+    author="Karnesh Jain",
+    author_email="Karnesh Jain_email",
+    url="",
+    license="MIT",
     packages=find_namespace_packages(),
-    package_data={'': resource_files},
+    package_data={"": resource_files},
     include_package_data=True,
     zip_safe=False,
     install_requires=dependencies,
